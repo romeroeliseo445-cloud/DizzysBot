201e3d06f2d4b084a859fab1115ad085b6f8000c
romeroeliseo445-cloud <romeroeliseo445@gmail.com>
2026-02-26 20:37:59 -0700

    Update index.js

--- index.js
+++ index.js
@@ -382,7 +382,17 @@ client.on('interactionCreate', async interaction => {
   }
 });
 
-client.login(process.env.TOKEN)
+// KEEP RENDER HAPPY ON FREE TIER
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('DizzyHub bot is alive 🟢');
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Web server running on port ${process.env.PORT || 3000}`);
+});client.login(process.env.TOKEN)
   .then(() => console.log('LOGIN SUCCESS - Bot should be online in 5-10 seconds'))
   .catch(err => {
     console.error('LOGIN CRASHED:', err.message || err);
@@ -391,4 +401,5 @@ client.login(process.env.TOKEN)
 
 client.on('error', err => console.error('CLIENT ERROR:', err.message || err));
 client.on('warn', warn => console.log('CLIENT WARNING:', warn));
+
 
